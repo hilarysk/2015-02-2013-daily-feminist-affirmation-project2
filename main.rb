15016beb7efdc53edc4503abcb7f719cb23e11a6
Hilary Stohs-Krause <hilarysk@gmail.com>
2015-03-16 15:55:48 -0500

    added database production code

--- main.rb
+++ main.rb
@@ -8,6 +8,22 @@ require "sinatra/activerecord"
 require "bcrypt"
 require "json"
 
+configure :development do
+set :database, {adapter: "sqlite3", database: "feminist_affirmation.db"}
+end
+
+configure :production do
+ db = URI.parse(ENV['DATABASE_URL'])
+ ActiveRecord::Base.establish_connection(
+ :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+ :host => db.host,
+ :username => db.user,
+ :password => db.password,
+ :database => db.path[1..-1],
+ :encoding => 'utf8'
+ )
+end
+
 require_relative "models/class-module.rb"
 require_relative "models/instance-module.rb"
 
@@ -25,9 +41,7 @@ require_relative "models/keyword_class.rb"
 require_relative "controllers/admin"
 require_relative "controllers/public"
 
-configure :development do
-set :database, {adapter: "sqlite3", database: "feminist_affirmation.db"}
-end
+
 
 
 #ADD VALIDATION FOR CLASSES
